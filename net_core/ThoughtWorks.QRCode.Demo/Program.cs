69693fe5f04abb0b0386e8269ef42d3b47c3fa5f
中国好程序员 <fromfire@qq.com>
2019-01-05 20:49:48 +0800

    ThoughtWorks.QRCode

--- net_core/ThoughtWorks.QRCode.Demo/Program.cs
+++ net_core/ThoughtWorks.QRCode.Demo/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ThoughtWorks.QRCode.Demo
 {
@@ -6,7 +10,17 @@ namespace ThoughtWorks.QRCode.Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = "D:/" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png";
+            var qr = QrCodeUtil.Encode("Hello World!");
+            qr.Save(path);
+            Console.WriteLine(QrCodeUtil.Decode(qr));
+
+            path = "D:/" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".png";
+            QrCodeUtil.Create("中国智造，惠及全球！", path);
+            Console.WriteLine(QrCodeUtil.Decode(path));
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
         }
     }
 }
