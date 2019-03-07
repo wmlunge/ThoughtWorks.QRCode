6990446643c98bc9b1461285100eed51814e0c5d
中国好程序员 <fromfire@qq.com>
2019-03-07 21:23:37 +0800

    add ThoughtWorks.QRCode.Test.NuGet

---
+++ net_core/ThoughtWorks.QRCode.Test.NuGet/QrCodeUtil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+
+namespace ThoughtWorks.QRCode.Test.NuGet
+{
+    /// <summary>
+    /// 二维码工具
+    /// </summary>
+    public static class QrCodeUtil
+    {
+        /// <summary>
+        /// 返回二维码图片
+        /// </summary>
+        public static Bitmap Encode(string text)
+        {
+            var qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeVersion = 5;
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
+            qrCodeEncoder.QRCodeScale = 4;
+            return qrCodeEncoder.Encode(text);
+        }
+
+        /// <summary>
+        /// 定义参数,生成二维码
+        /// </summary>
+        public static void Create(string text, string path)
+        {
+            Encode(text).Save(path);
+        }
+
+        /// <summary>
+        /// 返回二维码定义的字符串
+        /// </summary>
+        public static string Decode(Bitmap image)
+        {
+            var qrCodeBitmapImage = new QRCodeBitmapImage(image);
+            var qrCodeDecoder = new QRCodeDecoder();
+            return qrCodeDecoder.decode(qrCodeBitmapImage); ;
+        }
+
+        /// <summary>
+        /// 返回二维码定义的字符串
+        /// </summary>
+        public static string Decode(string path)
+        {
+            return Decode(new Bitmap(path));
+        }
+    }
+}
